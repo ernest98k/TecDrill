f855ade11ec61807e68afe7285ebe7a4d33e13a4
Eduardo <edug12631@gmail.com>
2019-12-16 18:02:09 +0000

    login-estrutura

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -41,89 +41,27 @@
   <!--/ Form Search Star /-->
   <div class="box-collapse">
     <div class="title-box-d">
-      <h3 class="title-d">Propriedade de Pesquisa</h3>
+      <h3 class="title-d">Login</h3>
     </div>
     <span class="close-box-collapse right-boxed ion-ios-close"></span>
     <div class="box-collapse-wrap form">
       <form class="form-a">
+        <br><br>
         <div class="row">
           <div class="col-md-12 mb-2">
             <div class="form-group">
-              <label for="Type">Pesquisa</label>
-              <input type="text" class="form-control form-control-lg form-control-a" placeholder="Palavra Chave">
+              <label for="Type">Email</label>
+              <input type="text" class="form-control form-control-lg form-control-a" placeholder="Email">
             </div>
           </div>
-          <div class="col-md-6 mb-2">
+          <div class="col-md-12 mb-2">
             <div class="form-group">
-              <label for="Type">Type</label>
-              <select class="form-control form-control-lg form-control-a" id="Type">
-                <option>All Type</option>
-                <option>For Rent</option>
-                <option>For Sale</option>
-                <option>Open House</option>
-              </select>
-            </div>
-          </div>
-          <div class="col-md-6 mb-2">
-            <div class="form-group">
-              <label for="city">City</label>
-              <select class="form-control form-control-lg form-control-a" id="city">
-                <option>All City</option>
-                <option>Alabama</option>
-                <option>Arizona</option>
-                <option>California</option>
-                <option>Colorado</option>
-              </select>
-            </div>
-          </div>
-          <div class="col-md-6 mb-2">
-            <div class="form-group">
-              <label for="bedrooms">Bedrooms</label>
-              <select class="form-control form-control-lg form-control-a" id="bedrooms">
-                <option>Any</option>
-                <option>01</option>
-                <option>02</option>
-                <option>03</option>
-              </select>
-            </div>
-          </div>
-          <div class="col-md-6 mb-2">
-            <div class="form-group">
-              <label for="garages">Garages</label>
-              <select class="form-control form-control-lg form-control-a" id="garages">
-                <option>Any</option>
-                <option>01</option>
-                <option>02</option>
-                <option>03</option>
-                <option>04</option>
-              </select>
-            </div>
-          </div>
-          <div class="col-md-6 mb-2">
-            <div class="form-group">
-              <label for="bathrooms">Bathrooms</label>
-              <select class="form-control form-control-lg form-control-a" id="bathrooms">
-                <option>Any</option>
-                <option>01</option>
-                <option>02</option>
-                <option>03</option>
-              </select>
-            </div>
-          </div>
-          <div class="col-md-6 mb-2">
-            <div class="form-group">
-              <label for="price">Min Price</label>
-              <select class="form-control form-control-lg form-control-a" id="price">
-                <option>Unlimite</option>
-                <option>$50,000</option>
-                <option>$100,000</option>
-                <option>$150,000</option>
-                <option>$200,000</option>
-              </select>
+              <label for="Type">Palavra-passe</label>
+              <input type="password" class="form-control form-control-lg form-control-a" placeholder="Palavra-Passe">
             </div>
           </div>
           <div class="col-md-12">
-            <button type="submit" class="btn btn-b">Pesquisar</button>
+            <button type="submit" class="btn btn-b">Entrar</button>
           </div>
         </div>
       </form>
@@ -141,9 +79,9 @@
         <span></span>
       </button>
       <a href="{{route("index")}}"><img src="{{asset('img/logo_tecdrill.png')}}" class="logo_image" alt="Logo"> </a>
-      <button type="button" class="btn btn-link nav-search navbar-toggle-box-collapse d-md-none" data-toggle="collapse"
+      <!--<button type="button" class="btn btn-link nav-search navbar-toggle-box-collapse d-md-none" data-toggle="collapse"
         data-target="#navbarTogglerDemo01" aria-expanded="false">
-        <span class="fa fa-search" aria-hidden="true"></span>
+        <span class="fa fa-search" aria-hidden="true"></span>-->
       </button>
       <div class="navbar-collapse collapse justify-content-center" id="navbarDefault">
         <ul class="navbar-nav">
@@ -163,14 +101,14 @@
           <a class="nav-link" href="{{route("recruitment")}}">Recrutamento</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{route("contact")}}"">Contactos</a>
+            <a class="nav-link" href="{{route("contact")}}">Contactos</a>
           </li>
         </ul>
       </div>
-      <!-- <button type="button" class="btn btn-b-n navbar-toggle-box-collapse d-none d-md-block" data-toggle="collapse"
+      <button type="button" class="btn btn-b-n navbar-toggle-box-collapse d-none d-md-block" data-toggle="collapse"
         data-target="#navbarTogglerDemo01" aria-expanded="false">
-        <span class="fa fa-search" aria-hidden="true"></span>
-      </button> -->
+        <span class="fa fa-user-circle" aria-hidden="true"></span>
+      </button>
     </div>
   </nav>
   <!--/ Nav End /-->

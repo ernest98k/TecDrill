0bdfd706c1ce99ebb13f32b6061289c1f22eb929
Eduardo <edug12631@gmail.com>
2020-01-17 17:11:36 +0000

    tabelas criadas!!
    criar database dbtecdrill no phpmyadmin  e fazer php artisan migrate

--- database/migrations/2020_01_17_162710_create_faqs_table.php
+++ database/migrations/2020_01_17_162710_create_faqs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddFieldsToUserTable extends Migration
+class CreateFaqsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,11 @@ class AddFieldsToUserTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->enum('role',['N', 'A'])->default('N');
-            $table->softDeletes();
+        Schema::create('faqs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('question');
+            $table->string('answer');
+            $table->timestamps();
         });
     }
 
@@ -26,8 +28,6 @@ class AddFieldsToUserTable extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('faqs');
     }
 }

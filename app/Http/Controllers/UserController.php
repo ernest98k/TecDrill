119b362b8f4f51d3a4d52504a96830c6f931fdb6
Eduardo <edug12631@gmail.com>
2020-01-28 01:04:14 +0000

    comentario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -110,7 +110,7 @@ class UserController extends Controller
      * @param  \App\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function destroy(User $user)
+    public function destroy(User $user)//caso haja problemas por id
     {
         $user->delete();
         return redirect()->route('users.index')->with('success', 'Utilizador eliminado com sucesso');
